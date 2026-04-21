96ffc5860eaf5ff1e938950ab88c09783de31415
Ilaria Piras <ilaria.piras2@studio.unibo.it>
2026-04-21 18:08:48 +0200

    Added function to check the Network status

--- app/src/main/java/com/example/progettoesame/data/SyncManager.kt
+++ app/src/main/java/com/example/progettoesame/data/SyncManager.kt
@@ -1,6 +1,8 @@
 package com.example.progettoesame.data
 
 import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import androidx.work.Constraints
 import androidx.work.ExistingWorkPolicy
 import androidx.work.NetworkType
@@ -8,6 +10,15 @@ import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 
 class SyncManager(private val context: Context) {
+
+    fun isOnline(): Boolean {
+        val connectivityManager = context
+            .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities = connectivityManager
+            .getNetworkCapabilities(connectivityManager.activeNetwork)
+        return capabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) == true ||
+                capabilities?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) == true
+    }
 
     fun triggerSync() {
         val constraints = Constraints.Builder()
