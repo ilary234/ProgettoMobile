f61b784acba9d63a58c7781d640fc7b316375a7f
Ilaria Piras <ilaria.piras2@studio.unibo.it>
2026-04-21 13:40:50 +0200

    Added type converters for Ingredients and Steps

--- app/src/main/java/com/example/progettoesame/data/database/ProjectDatabase.kt
+++ app/src/main/java/com/example/progettoesame/data/database/ProjectDatabase.kt
@@ -2,9 +2,11 @@ package com.example.progettoesame.data.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.progettoesame.data.database.daos.RecipeDAO
 
 @Database([Category::class], version = 1)
+@TypeConverters(TypeConverters::class)
 abstract class ProjectDatabase : RoomDatabase() {
     abstract fun RecipeDAO(): RecipeDAO
     //Aggiungere la abstract anche per gli altri DAO
---
+++ app/src/main/java/com/example/progettoesame/data/database/TypeConverters.kt
@@ -0,0 +1,20 @@
+package com.example.progettoesame.data.database
+
+import androidx.room.TypeConverter
+import kotlinx.serialization.json.Json
+
+class TypeConverters {
+    private val json = Json { ignoreUnknownKeys = true }
+
+    @TypeConverter
+    fun fromIngredientList(value: List<Ingredient>): String = json.encodeToString(value)
+
+    @TypeConverter
+    fun toIngredientList(value: String): List<Ingredient> = json.decodeFromString(value)
+
+    @TypeConverter
+    fun fromStepList(value: List<Step>): String = json.encodeToString(value)
+
+    @TypeConverter
+    fun toStepList(value: String): List<Step> = json.decodeFromString(value)
+}
