b26cf2c153ff6d68c404bcf74ebeb8a97f6d7976
Ilaria Piras <ilaria.piras2@studio.unibo.it>
2026-04-21 13:26:52 +0200

    Added a queries and function for sync of recipes

--- app/src/main/java/com/example/progettoesame/data/database/Entities.kt
+++ app/src/main/java/com/example/progettoesame/data/database/Entities.kt
@@ -5,6 +5,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 @Serializable
 @Entity(tableName = "categories")
@@ -13,7 +14,8 @@ data class Category (
     @SerialName("category_id")
     val categoryId : String,
     @SerialName("name") val name : String,
-    @SerialName("order") val order : Int
+    @SerialName("order") val order : Int,
+    @Transient val isSynced: Boolean = false
 )
 
 @Serializable
@@ -27,7 +29,8 @@ data class User (
     @SerialName("email") val email : String,
     @SerialName("recipe_number") val recipeNumber : Int = 0,
     @SerialName("average_rating") val averageRating : Float = 0.0f,
-    @SerialName("updated_at") val updatedAt : String
+    @SerialName("updated_at") val updatedAt : String,
+    @Transient val isSynced: Boolean = false
 )
 
 @Serializable
@@ -43,7 +46,8 @@ data class Ingredient (
     @SerialName("quantity") val quantity : Float,
     @SerialName("unit") val unit : String, //enum di kg, g, ml, L
     @SerialName("updated_at") val updatedAt : String,
-    @SerialName("is_deleted") val isDeleted: Boolean = false
+    @SerialName("is_deleted") val isDeleted: Boolean = false,
+    @Transient val isSynced: Boolean = false
 )
 
 @Serializable
@@ -52,7 +56,8 @@ data class Step (
     val imageUrls: List<String>,
     @SerialName("description") val description : String,
     @SerialName("updated_at") val updatedAt : String,
-    @SerialName("is_deleted") val isDeleted: Boolean = false
+    @SerialName("is_deleted") val isDeleted: Boolean = false,
+    @Transient val isSynced: Boolean = false
 )
 
 @Serializable
@@ -69,7 +74,8 @@ data class Recipe (
     val steps : List<Step>,
     @SerialName("average_rating") val averageRating : Float = 0.0f,
     @SerialName("updated_at") val updatedAt : String,
-    @SerialName("is_deleted") val isDeleted: Boolean = false
+    @SerialName("is_deleted") val isDeleted: Boolean = false,
+    @Transient val isSynced: Boolean = false
 )
 
 @Serializable
@@ -78,5 +84,6 @@ data class UserFavourite (
     @SerialName("user_id") val userId: String,
     @SerialName("recipe_id") val recipeId : String,
     @SerialName("updated_at") val updatedAt : String,
-    @SerialName("is_deleted") val isDeleted: Boolean = false
+    @SerialName("is_deleted") val isDeleted: Boolean = false,
+    @Transient val isSynced: Boolean = false
 )
--- app/src/main/java/com/example/progettoesame/data/database/daos/RecipeDAO.kt
+++ app/src/main/java/com/example/progettoesame/data/database/daos/RecipeDAO.kt
@@ -1,8 +1,26 @@
 package com.example.progettoesame.data.database.daos
 
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import com.example.progettoesame.data.database.Recipe
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RecipeDAO {
-    //TODO
+
+    @Query("SELECT * FROM recipes WHERE isSynced = 0")
+    suspend fun getUnsyncedRecipes(): List<Recipe>
+
+    @Query("UPDATE recipes SET isSynced = 1 WHERE recipeId = :id")
+    suspend fun markAsSynced(id: String)
+
+    @Query("SELECT MAX(updatedAt) FROM recipes")
+    suspend fun getLastUpdateTimestamp(): String?
+
+    @Upsert
+    suspend fun upsertAll(recipes: List<Recipe>)
+
+    @Upsert
+    suspend fun upsert(recipe: Recipe)
 }
