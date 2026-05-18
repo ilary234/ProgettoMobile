232a89278d5bda8d1a01247d1f8b6a324c9c469d
Ilaria Piras <ilaria.piras2@studio.unibo.it>
2026-05-18 17:32:57 +0200

    Created utility composable RecipePreviewCard

--- app/src/main/java/com/example/progettoesame/ui/screens/CategoryScreen.kt
+++ app/src/main/java/com/example/progettoesame/ui/screens/CategoryScreen.kt
@@ -1,24 +1,15 @@
 package com.example.progettoesame.ui.screens
 
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -33,7 +24,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -42,10 +32,8 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import com.example.progettoesame.ui.NavigationRoute
 import com.example.progettoesame.ui.utils.AuthState
-import com.example.progettoesame.ui.utils.InfoPreview
 import com.example.progettoesame.ui.utils.LoginRequiredDialog
-import com.example.progettoesame.ui.utils.PreviewCard
-import com.example.progettoesame.ui.utils.formatTime
+import com.example.progettoesame.ui.utils.RecipePreviewCard
 import com.example.progettoesame.ui.viewmodels.CategoryViewModel
 
 
@@ -111,43 +99,9 @@ fun CategoryScreen(navController: NavController,
                 items(recipesState.recipes.entries.toList(), key = { it.key.recipeId }) {
                     val recipe = it.key
                     val isFavorite = it.value
-                    Column(
-                        modifier = Modifier.fillMaxWidth()
-                            .padding(horizontal = 8.dp)
-                            .background(color = Color(0xfff7ead0), shape = RoundedCornerShape(16.dp))
-                            .padding(16.dp)
-                            .clickable{
-                                navController.navigate(NavigationRoute.RecipeDetails(recipe.recipeId))
-                            },
-                        verticalArrangement = Arrangement.spacedBy(8.dp)
-                    ) {
-
-                        PreviewCard(recipe.previewImageUrl, recipe.title)
-
-                        val time = recipe.preparation + recipe.cooking + (recipe.waiting ?: 0)
-                        InfoPreview(recipe.title, formatTime(time), recipe.averageRating)
-
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.End,
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            IconButton(onClick = {
-                                if (AuthState.isLoggedIn.value) {
-                                    categoryViewModel.actions.onFavorite(recipe, AuthState.userId.value!!)
-                                } else {
-                                    showDialog = true
-                                }
-                            }) {
-                                Icon(
-                                    imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
-                                    contentDescription = "Favorite Icon",
-                                    modifier = Modifier.size(24.dp),
-                                    tint = if (isFavorite) Color.Red else Color.Gray
-                                )
-                            }
-                        }
-                    }
+                    RecipePreviewCard({navController.navigate(NavigationRoute.RecipeDetails(recipe.recipeId))},
+                    recipe, isFavorite, {categoryViewModel.actions.onFavorite(recipe, AuthState.userId.value!!)},
+                        {showDialog = true})
                 }
             }
         }
